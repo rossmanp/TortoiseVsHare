5d0de17eb29a770e15079941eb83298268518336
Pete Rossman <pete.rossman@gmail.com>
2017-04-10 15:19:46 -0400

    First commit

---
+++ GCBonusLab_TortoiseVsHare/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCBonusLab_TortoiseVsHare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadRunner race = new ThreadRunner();
+            ThreadRunner tortoise = new ThreadRunner("Tortoise", 100, 10);
+            ThreadRunner hare = new ThreadRunner("Hare", 10, 100);
+            Console.WriteLine("The tortoise and hare are having a race!");
+            bool DidTortoiseWin = race.Run(tortoise, hare);
+            if (DidTortoiseWin == true)
+            {
+                Console.WriteLine("Tortoise wins!");
+            }
+            else
+            {
+                Console.WriteLine("Hare wins!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GCBonusLab_TortoiseVsHare/ThreadRunner.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace GCBonusLab_TortoiseVsHare
+{
+    public class ThreadRunner
+    {
+        public string _name { get; private set; }
+        public int _move { get; private set; }
+        public int _speed { get; private set; }
+        private int _distance = 0;
+        Random rnd = new Random();
+
+        public ThreadRunner()
+        {
+
+        }
+
+        public ThreadRunner(string name, int move, int speed)
+        {
+            this._name = name;
+            this._move = move;
+            this._speed = speed;           
+        }
+
+        public bool Run(ThreadRunner tortoise, ThreadRunner hare)
+        {
+            int chance = 0;
+            for (int i = 1; tortoise._distance < 1000; i++)
+            {
+                chance = rnd.Next(1, 101);
+                if (chance <= tortoise._move)
+                {
+                    tortoise._distance += tortoise._speed;
+                }
+                if (chance <= hare._move)
+                {
+                    hare._distance += hare._speed;
+                }              
+                Console.WriteLine("\nThe tortoise has moved " + tortoise._distance + " meters and the hare has moved " + hare._distance + " meters.");
+                Thread.Sleep(100);
+                if (hare._distance >= 1000)
+                {
+                    break;
+                }
+            }
+            if (tortoise._distance >= 1000)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
